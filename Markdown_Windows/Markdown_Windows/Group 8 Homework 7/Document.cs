f2a14f72f3ffbad0942cd2337bf71f3297e9f9c2
Bernardo Pla <bpla2112@gmail.com>
2017-07-17 17:30:26 -0400

    Commit of all relevant code
    All C# code for this repository

---
+++ Markdown_Windows/Markdown_Windows/Group 8 Homework 7/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_8_Homework_7
+{
+    [Serializable]
+    public class Document
+    {
+        public Shape[] ShapeList { get; set; }
+
+        Document() { }
+    }
+  }
+      
